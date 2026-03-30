8a1b088f763da4aa58bc99bdb80d6692c9eb8eb8
Tamir Yamaleyev <tamirka8@gmail.com>
2026-03-30 06:13:39 +0300

    Connected quests to quest tracker

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -13,6 +13,7 @@ public class QuestManager : MonoBehaviour
     // Events
     public delegate void QuestEvent(Quest quets);
     public event QuestEvent OnQuestStarted;
+    public event QuestEvent OnQuestProgressUpdated;
     public event QuestEvent OnQuestProgressCompleted;
     public event QuestEvent OnQuestTurnedIn;
 
@@ -49,6 +50,7 @@ public class QuestManager : MonoBehaviour
 
         bool wasCompleted = quest.IsCompleted;
         quest.AddProgress(amount);
+        OnQuestProgressUpdated?.Invoke(quest);
 
         if (!wasCompleted && quest.IsCompleted)
             OnQuestProgressCompleted?.Invoke(quest);
